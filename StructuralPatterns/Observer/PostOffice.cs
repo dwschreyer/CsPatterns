bf4b710ebe4b0eb8cbfbca7fb6b74eeefdc8ea14
SUPERSAURUS\dwsch <dwschreyer@live.com>
2019-07-03 19:33:36 +1200

    Started Structural Patterns.

---
+++ StructuralPatterns/Observer/PostOffice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralPatterns.Observer
+{
+    public class PostOffice : IObservable<Message>
+    {
+        public PostOffice()
+        {
+
+        }
+
+        public IList<IObserver<Message>> Observers { get; set; }
+
+        public IDisposable Subscribe(IObserver<Message> observer)
+        {
+            if(!Observers.Contains(observer))
+            {
+                Observers.Add(observer);
+            }
+            return new Unsubscriber(Observers, observer);
+        }
+    }
+}
---
+++ StructuralPatterns/Observer/Unsubscriber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructuralPatterns.Observer
+{
+    public class Unsubscriber : IDisposable
+    {
+        private IObserver<Message> _observer;
+        private IList<IObserver<Message>> _observers;
+        public Unsubscriber(IList<IObserver<Message>> observers, IObserver<Message> observer)
+        {
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observer != null && _observers.Contains(_observer))
+            {
+                _observers.Remove(_observer);
+            }
+        }
+    }
+}
