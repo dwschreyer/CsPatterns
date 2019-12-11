767a220384f1a6c4713a99d79968dc1927644a9d
Darrel Schreyer <Darrel.Schreyer@serko.com>
2019-12-12 10:43:07 +1300

    Added Configuration examples.

---
+++ DotNetCoreBasics.ConfigurationCli/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace DotNetCoreBasics.ConfigurationCli
+{
+    class Program
+    {
+        static readonly string[] _keys = new[] { "appName" };
+        static readonly string _breakLine = new string('=', 50);
+        static IConfiguration _configuration;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Configuration Demo" + Environment.NewLine);
+
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            WriteOutput();
+
+            Console.WriteLine("Environment Output" + Environment.NewLine);
+            foreach (var key in _keys)
+            {
+                Console.WriteLine($"\t{key}    : {_configuration[key]}");
+                Console.WriteLine(Environment.NewLine);
+            }
+            Console.WriteLine(_breakLine);
+        }
+
+        private static void WriteOutput()
+        {
+            foreach (var key in _keys)
+            {
+                Console.WriteLine(_breakLine);
+                Console.WriteLine(key + Environment.NewLine);
+
+                foreach (var provider in ((IConfigurationRoot)_configuration).Providers)
+                {
+                    if (provider.TryGet(key, out string value))
+                    {
+                        Console.WriteLine($"\tProvider :{provider.GetType().Name}");
+                        Console.WriteLine($"\tValue    :{value}");
+                        Console.WriteLine(Environment.NewLine);
+                    }
+                }
+            }
+            Console.WriteLine(_breakLine);
+        }
+    }
+}
