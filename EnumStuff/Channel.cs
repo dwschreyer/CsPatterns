713117bf4fd1168bf49b6049a0beef35b22c1559
Darrel Schreyer <dwschreyer@live.com>
2020-01-03 21:09:46 +1300

    Enum Stuff.

---
+++ EnumStuff/Channel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EnumStuff
+{
+    [Flags]
+    public enum Channel
+    {
+        None = 0b0000,
+        Email = 0b0001,
+        Text = 0b0010,
+        Voice = 0b0100,
+        Billboard = 0b1000,
+        Radio = 0b0001_0000,
+        Television = 0b0010_0000,
+
+        All = int.MaxValue
+    }
+}
---
+++ EnumStuff/EnumFlagUsage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumStuff
+{
+    public static class EnumFlagUsage
+    {
+        public static void Use()
+        {
+            var customerChannels = new Dictionary<string, Channel>()
+            {
+                { "Customer 1", Channel.Email | Channel.Text },
+                { "Customer 2", Channel.Email | Channel.Voice }
+            };
+
+            foreach (var customerChannel in customerChannels)
+            {
+                var hasText = (customerChannel.Value & Channel.Text) == Channel.Text;
+                Console.WriteLine($"{customerChannel.Key} : Has Text: {hasText}");
+            }
+        }
+    }
+}
+
+public abstract class A
+{
+    public A(string name)
+    {
+
+    }
+}
+
+public class B : A
+{
+    public B() : base("From B")
+    {
+
+    }
+}
